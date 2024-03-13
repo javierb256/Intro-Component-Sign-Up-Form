aa9f802a1c00930fd7a737a3b95f869cea349d44
= <44247277+javierb256@users.noreply.github.com>
2024-03-12 22:42:58 -0700

    worked on form

--- script.js
+++ script.js
@@ -3,10 +3,32 @@ const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const button = document.querySelector('button');
+const errors = document.querySelector('.hidden');
 
-button.addEventListener('click', ()=> {
+// fix buttons staying red after clicking button
+// add email validation
+
+button.addEventListener('click', (e)=> {
+    e.preventDefault();
     if(firstName.value == "" || lastName.value == "" || email.value == "" || password.value == ""){
-        alert('please fill in every field');
+        errors.classList.toggle("hidden");
+    }
+
+    if(firstName.value == "") {
+        firstName.classList.toggle("errorBorder");
+    }
+    if(lastName.value == "") {
+        lastName.classList.toggle("errorBorder");
+    }
+    if(email.value == "") {
+        email.classList.toggle("errorBorder");
+    }
+    if(password.value == "") {
+        password.classList.toggle("errorBorder")
+    }
+
+    else {
+        firstName
     }
     
 })
