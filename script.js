7820b7f21413ee280fc8ff6f42101a7714451480
= <44247277+javierb256@users.noreply.github.com>
2024-03-13 22:05:09 -0700

    fixed inputs displaying errorBorder incorrectly. Added email validation

--- script.js
+++ script.js
@@ -4,31 +4,46 @@ const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const button = document.querySelector('button');
 const errors = document.querySelector('.hidden');
-
-// fix buttons staying red after clicking button
-// add email validation
+const emailError = document.querySelector(".emailError");
+const inputsArray = document.querySelectorAll('input');
 
 button.addEventListener('click', (e)=> {
     e.preventDefault();
     if(firstName.value == "" || lastName.value == "" || email.value == "" || password.value == ""){
-        errors.classList.toggle("hidden");
+        errors.classList.remove("hidden");
+    }
+
+    if(firstName.value != "" && lastName.value != "" && email.value != "" && password.value != ""){
+        errors.classList.add("hidden");
     }
 
     if(firstName.value == "") {
-        firstName.classList.toggle("errorBorder");
+        firstName.classList.add("errorBorder");
     }
     if(lastName.value == "") {
-        lastName.classList.toggle("errorBorder");
+        lastName.classList.add("errorBorder");
     }
     if(email.value == "") {
-        email.classList.toggle("errorBorder");
+        email.classList.add("errorBorder");
+    }
+    if(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == false){
+        email.classList.add("errorBorder");
+    }
+    if(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == true){
+        emailError.classList.add("hidden");
     }
     if(password.value == "") {
-        password.classList.toggle("errorBorder")
+        password.classList.add("errorBorder");
     }
 
-    else {
-        firstName
-    }
     
+})
+
+inputsArray.forEach(input => {
+    input.addEventListener('keydown', ()=>{
+        if(input.classList == 'errorBorder'){
+            input.classList.toggle('errorBorder');
+        }
+
+    })
 })
