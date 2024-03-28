ca2d94e67b9deb849b1ec170d624f7454adc7783
= <44247277+javierb256@users.noreply.github.com>
2024-03-27 22:30:46 -0700

    finished finall readme

--- script.js
+++ script.js
@@ -1,67 +1,76 @@
-const firstName = document.querySelector('#firstName');
+const firstName = document.querySelector("#firstName");
 const firstNameEmpty = document.querySelector(".firstNameEmpty");
-const lastName = document.querySelector('#lastName');
+const lastName = document.querySelector("#lastName");
 const lastNameEmpty = document.querySelector(".lastNameEmpty");
-const email = document.querySelector('#email');
+const email = document.querySelector("#email");
 const emailEmpty = document.querySelector(".emailEmpty");
-const password = document.querySelector('#password');
+const password = document.querySelector("#password");
 const passwordEmpty = document.querySelector(".passwordEmpty");
-const button = document.querySelector('button');
-const error = document.querySelector('.error');
+const button = document.querySelector("button");
+const error = document.querySelector(".error");
 const emailError = document.querySelector(".emailError");
-const inputsArray = document.querySelectorAll('input');
+const inputsArray = document.querySelectorAll("input");
 
-firstName.value = ""
-lastName.value = ""
-email.value = ""
-password.value = ""
-emailError.style.display = "none"
+firstName.value = "";
+lastName.value = "";
+email.value = "";
+password.value = "";
+emailError.style.display = "none";
 
-button.addEventListener('click', (e)=> {
-    e.preventDefault();
-    if(firstName.value == "" || lastName.value == "" || email.value == "" || password.value == ""){
-        error.style.display= "block";
-    }
+button.addEventListener("click", e => {
+  e.preventDefault();
+  if (
+    firstName.value == "" ||
+    lastName.value == "" ||
+    email.value == "" ||
+    password.value == ""
+  ) {
+    error.style.display = "block";
+  }
 
-    if(firstName.value != "" && lastName.value != "" && email.value != "" && password.value != ""){
-        error.style.display = "none";
-    }
+  if (
+    firstName.value != "" &&
+    lastName.value != "" &&
+    email.value != "" &&
+    password.value != ""
+  ) {
+    error.style.display = "none";
+  }
 
-    if(firstName.value == "") {
-        firstNameEmpty.classList.remove("hidden");
-        firstName.classList.add("errorBorder");
-    }
-    if(lastName.value == "") {
-        lastNameEmpty.classList.remove("hidden");
-        lastName.classList.add("errorBorder");
-    }
-    if(email.value == "") {
-        emailEmpty.classList.remove("hidden");
-        email.classList.add("errorBorder");
-    }
-    // email incorrectly formatted
-    if(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == false && email.value != ""){
-        emailError.style.display="block";
-        email.classList.add("errorBorder");
-    }
-    // email correctly formatted
-    if(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == true){
-        emailError.style.display = "none";
-    }
-    if(password.value == "") {
-        passwordEmpty.classList.remove("hidden");
-        password.classList.add("errorBorder");
-    }
-
-    
-})
+  if (firstName.value == "") {
+    firstNameEmpty.classList.remove("hidden");
+    firstName.classList.add("errorBorder");
+  }
+  if (lastName.value == "") {
+    lastNameEmpty.classList.remove("hidden");
+    lastName.classList.add("errorBorder");
+  }
+  if (email.value == "") {
+    emailEmpty.classList.remove("hidden");
+    email.classList.add("errorBorder");
+  }
+  // email incorrectly formatted
+  if (
+    /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == false &&
+    email.value != ""
+  ) {
+    emailError.style.display = "block";
+    email.classList.add("errorBorder");
+  }
+  // email correctly formatted
+  if (/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(email.value) == true) {
+    emailError.style.display = "none";
+  }
+  if (password.value == "") {
+    passwordEmpty.classList.remove("hidden");
+    password.classList.add("errorBorder");
+  }
+});
 inputsArray.forEach(input => {
-    input.addEventListener('keydown', ()=>{
-        if(input.classList == 'errorBorder'){
-           
-            input.classList.toggle('errorBorder');
-            input.nextElementSibling.classList.add("hidden")
-        }
-
-    })
-})
+  input.addEventListener("keydown", () => {
+    if (input.classList == "errorBorder") {
+      input.classList.toggle("errorBorder");
+      input.nextElementSibling.classList.add("hidden");
+    }
+  });
+});
